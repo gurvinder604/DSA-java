ef4be59b8ec05d89dc2447b728efc8c909cc4c0f
Gurvinder Singh (Garry) <gurvinder604singh@gmail.com>
2024-10-19 21:14:39 +0100

    c2

--- Binary Search/src/BinarySearch.java
+++ Binary Search/src/BinarySearch.java
@@ -1,2 +1,5 @@
 public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
 }
--- Linear Search/src/findEvenDigits.java
+++ Linear Search/src/findEvenDigits.java
@@ -1,2 +1,32 @@
 public class findEvenDigits {
+    public static void main (String[] args ) {
+        int[] array =  {12,2,122,32,6543,4,675,8};
+        int ans = FindEvenDigits(array);
+        System.out.println(ans);
+    }
+
+    static int FindEvenDigits(int[] arr) {
+        int count = 0;
+
+        for (int num: arr) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // to check whether the num contains even digits
+    static boolean even(int num) {
+        return digits(num) % 2 == 0;
+    }
+
+    static int digits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num/10;
+        }
+        return count;
+    }
 }
